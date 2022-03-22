7e1435b835d2b751eb764ef243f6038f4a392f44
Tom Mong <mongyin98@gmail.com>
2022-03-22 14:22:30 +0800

    Some bug in Eastmoney

--- src/tests/test_eastmoney.py
+++ src/tests/test_eastmoney.py
@@ -1,11 +1,11 @@
 import unittest
 from lurkers import *
 
-# class TestEastMoneyLurkerMethods(unittest.TestCase):
+class TestEastMoneyLurkerMethods(unittest.TestCase):
 
-#     def test_canScrapeHK(self):
-#         lurker = EastMoney(1)
-#         self.assertTrue(lurker.dryrun())
+    def test_canScrapeHK(self):
+        lurker = EastMoney(1)
+        self.assertTrue(lurker.dryrun())
 
 if __name__ == '__main__':
     unittest.main()
